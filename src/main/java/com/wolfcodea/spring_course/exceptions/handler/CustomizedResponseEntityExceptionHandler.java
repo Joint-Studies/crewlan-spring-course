9e13319637e599b8d0220790028b7158565a86ae
Rodrigo Silva <rodrigoa.work@gmail.com>
2024-01-08 22:32:19 -0300

    add jwt to project, create services and models

--- src/main/java/com/wolfcodea/spring_course/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/wolfcodea/spring_course/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.wolfcodea.spring_course.exceptions.ExceptionResponse;
+import com.wolfcodea.spring_course.exceptions.InvalidJwtAuthenticationException;
 import com.wolfcodea.spring_course.exceptions.RequiredObjectsIsNullException;
 import com.wolfcodea.spring_course.exceptions.ResourceNotFoundException;
 
@@ -52,6 +53,18 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(
+			Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 
 }
